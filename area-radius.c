88b50ae5e1022cdc8e9758fe86422c83b723d191
Kushang Tanawala <kushangtanawala24@gmail.com>
2024-06-21 09:57:01 +0530

    commit nine

---
+++ area-radius.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+//area of radius
+int main() {
+    float radius;
+    printf("enter radius");
+    scanf("%f",&radius);
+    printf("area is : %f",3.14 * radius * radius);
+    return 0;
+}
